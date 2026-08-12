489309e3286429b9fb21e94673593c3b0af22490
changying00 <dgx2541104422@gamil.com>
2026-08-12 22:10:37 +0800

    8.12计划

--- 2026/2026.8/8.12/1.元类.py
+++ 2026/2026.8/8.12/1.元类.py
@@ -1,5 +1,32 @@
-"""假入有一个类、且该类中 定义了多个 类属性， 现要求编写一个元类、实现将类中的类型的类属性 全部更改为 大写属性，例如
+"""假入有一个类、且该类中 定义了多个 类属性， 现要求编写一个元类、实现将类中的类型的类属性
+全部更改为 大写属性，例如
 class A:
 	name = “a”
 	sex  = “m”
 经过元类处理后，A.NAME 能够输出 a , A.SEX 能够输出 m"""
+class UpperMeta(type):
+
+    def __new__(mcs, name, bases, namespace):
+
+        # 遍历类的命名空间
+        for key, value in list(namespace.items()):
+
+            # 如果属性值是字符串
+            if isinstance(value, str):
+                # 原来的属性删除
+                del namespace[key]
+
+                # 属性名改成大写
+                namespace[key.upper()] = value
+
+        # 创建类
+        return super().__new__(mcs, name, bases, namespace)
+
+
+class A(metaclass=UpperMeta):
+    name = "a"
+    sex = "m"
+
+
+print(A.NAME)
+print(A.SEX)
--- 2026/2026.8/8.12/2.元类创建一个计数器.py
+++ 2026/2026.8/8.12/2.元类创建一个计数器.py
@@ -10,3 +10,41 @@
 
 创建多个 Example 实例，并验证 CountingMeta 能正确返回实例数量。
 """
+class CountingMeta(type):
+    # 保存每个类的实例数量
+    instance_counts = {}
+
+    def __new__(mcs, name, bases, namespace):
+        # 创建类
+        cls = super().__new__(mcs, name, bases, namespace)
+
+        # 初始化该类的实例数量
+        mcs.instance_counts[cls] = 0
+
+        return cls
+
+    def __call__(cls, *args, **kwargs):
+        # 当 Example() 被调用时，会执行这里
+        instance = super().__call__(*args, **kwargs)
+
+        # 对当前类的实例数量 +1
+        CountingMeta.instance_counts[cls] += 1
+
+        return instance
+
+    @staticmethod
+    def get_instance_count(cls):
+        return CountingMeta.instance_counts.get(cls, 0)
+
+
+class Example(metaclass=CountingMeta):
+    pass
+
+
+# 创建实例
+a = Example()
+b = Example()
+c = Example()
+
+# 查看实例数量
+print(CountingMeta.get_instance_count(Example))
