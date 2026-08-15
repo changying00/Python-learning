f7f347ef5bbe74109aa6dfca5172245d53cebd3c
changying00 <dgx2541104422@gmail.com>
2026-08-15 15:13:32 +0800

    8.15作业

--- 2026/2026.8/week1/困难题/2.递归-编写一个函数、删除目录.py
+++ 2026/2026.8/week1/困难题/2.递归-编写一个函数、删除目录.py
@@ -5,25 +5,17 @@ from typing import List
 import  os
 
 def remove_contents(directory:str) ->List[str]:
-    """获取一个 指定目录下的所有内容"""
-
-    #存储 所有的资源路径
-    files = []
+    """删除 指定目录下的所有内容"""
     #获取该目录 下的所有内容
     contents = os.listdir(directory)
-    if contents == "":
-        os.rmdir(directory)
-    else:
-        for name in contents:
-            #获取name、代表的资源 它的完整路径
-            path = os.path.abspath(os.path.join(directory, name))
-            #判断 当前路径是文件还是文件
-            if os.path.isdir(path):
-                #递归获取 该目录下的所有内容
-                os.remove(path)
-            else:
-                remove_contents(path)
+    for name in contents:
+        path = os.path.join(directory, name)
 
+        if os.path.isdir(path):
+            remove_contents(path)
+        else:
+            os.remove(path)
+    os.rmdir(directory)
 
 if __name__ == "__main__":
-    files =remove_contents("")
+    files =remove_contents(r"C:\Users\QK\Desktop\python\remove")
--- 2026/2026.8/week1/简单题/1.FileUtil 类-静态方法.py
+++ 2026/2026.8/week1/简单题/1.FileUtil 类-静态方法.py
@@ -2,11 +2,19 @@
 """
 import os
 class FileUtils:
-
+from typing import Callable, List
     @classmethod
     def get_files(cls, path: str,  predicate: Callable[[str], bool] = None) -> List[str]:
         """根据 predicate条件获取指定目录下所有满足条件的文件"""
-        
+        files = []
+        for name in os.listdir(path):
+            file_path = os.path.join(path, name)
+            if os.path.isfile(file_path):
+                if predicate is None or predicate(file_path):
+                    files.append(file_path)
+            elif os.path.isdir(file_path):
+                files.extend(cls.get_files(file_path, predicate))
+        return files
 
     @classmethod
     def remove(cls, path: str) -> None:
@@ -43,8 +51,7 @@ class FileUtils:
     @classmethod
     def is_file(cls, path: str) -> bool:
         """判断磁盘路径是否是 文件"""
-        data = os.path.basename(path)
-        return os.path.isfile(data)
+        return os.path.isfile(path)
     @classmethod
     def exists(cls, path: str) -> bool:
         """判断路径是否存在 """
@@ -53,35 +60,56 @@ class FileUtils:
     @classmethod
     def get_ext(cls, file: str) -> str:
         """获取 文件 后缀名"""
-        return os.path.basename(file).split(".")[-1]
+        return os.path.basename(file)[1]
 
     @classmethod
     def copy_file_to_dir(cls, file: str,  directory: str)-> None:
         """拷贝一个文件到目录"""
-
+        filename = cls.get_name(file)
+        destfile = os.path.join(directory, filename)
+        cl s.copy_file(file, destfile)
     @classmethod
     def copy_file(cls, srcfile: str,  destfile: str) -> None:
         """拷贝一个文件内容到 destfile文件中"""
-        pass 
-
+        with open(srcfile, "rb") as src:
+            with open(destfile, "wb") as dest:
+                while True:
+                    data = src.read(8192)
+                    if not data:
+                        break
+                    dest.write(data)
     @classmethod
     def copy_directory(cls, src: str,  dest: str) -> None : 
         """将 src 文件夹中的内容拷贝到 dest 目录中 """
-        pass 
+        if not os.path.exists(dest):
+            os.mkdir(dest)
+        for name in os.listdir(src):
+            src_path = os.path.join(src, name)
+            dest_path = os.path.join(dest, name)
+            if os.path.isfile(src_path):
+                cls.copy_file(src_path, dest_path)
+            elif os.path.isdir(src_path):
+                cls.copy_directory(src_path, dest_path)
 
     @classmethod
     def read_to_string(cls, file: str, encoding="utf-8") -> None:
         """读取一个字符文件中的内容，并返回字符串"""
-        pass 
+        with open(file, "rt", encoding=encoding) as f:
+            return f.read()
 
     @classmethod
     def chunks(cls, file: str,  chunk_size: int=8192) -> None:
         """编写一个生成器、用来读取一个大文件中的内容、每次读取 chunk_size 字节"""
-        pass
+        with open(file, "rb") as f:
+            while True:
+                data = f.read(chunk_size)
+                if not data:
+                    break
+                yield data
 
     @classmethod
     def writestr(cls, string: str,  file: str, encoding="utf-8") -> None:
         """将指定的字符串写入到指定的文件中"""
-        pass
+        with open(file, "wt", encoding=encoding) as f:
+            f.write(string)
 
-"""
